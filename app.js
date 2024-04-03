d435f8853ca8655aee12a98b6d1106040997860d
erik-pole <101648330+erik-pole@users.noreply.github.com>
2024-04-03 09:50:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 function run(){
-  console.log('hej4');
+  console.log('hej5');
   try{
     jsforce.browser.init({
       clientId: '3MVG9xj60O9CjKHqAR6xaaX9u0JgiHcSo.P3c.i3Z6SRtOgiwUxIOQvy6EgSbassifvmoB8Gvt722lSqpIUq0',
-      redirectUri: 'https://test.salesforce.com'
+      redirectUri: 'https://erik-pole.github.io/eqtLightningOutTest/'
     });
     jsforce.browser.login();
+    jsforce.browser.on('connect', function(connection) {
+      console.log('Connected to Salesforce!');
+      // Access token is available in connection.accessToken
+      let accessToken = connection.accessToken;
+      console.log(accessToken);
+      // Call the function to render your Lightning Out component with the access token
+      //renderLightningOutComponent({ accessToken: accessToken });
+    });
   }
   catch(error){console.error(error)}
 }

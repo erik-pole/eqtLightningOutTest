e8df509ba07494cba4122a8fde183f3f1a07dfcf
erik-pole <101648330+erik-pole@users.noreply.github.com>
2024-04-03 09:34:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function run(){
   console.log('hej3');
   try{
-  jsforce.browser.login();
+    jsforce.browser.init({
+      clientId: '3MVG9xj60O9CjKHqAR6xaaX9u0JgiHcSo.P3c.i3Z6SRtOgiwUxIOQvy6EgSbassifvmoB8Gvt722lSqpIUq0',
+      redirectUri: 'https://test.salesforce.com'
+    });
   }
   catch(error){console.error(error)}
 }

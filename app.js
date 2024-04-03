46304a89743b8a62b3c9f571069d74e1c4fac579
erik-pole <101648330+erik-pole@users.noreply.github.com>
2024-04-03 09:36:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function run(){
       clientId: '3MVG9xj60O9CjKHqAR6xaaX9u0JgiHcSo.P3c.i3Z6SRtOgiwUxIOQvy6EgSbassifvmoB8Gvt722lSqpIUq0',
       redirectUri: 'https://test.salesforce.com'
     });
+    jsforce.browser.login();
   }
   catch(error){console.error(error)}
 }

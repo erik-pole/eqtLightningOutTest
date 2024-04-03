264fa1155438a6e92f122fbda5df1f6d03f5d21c
erik-pole <101648330+erik-pole@users.noreply.github.com>
2024-04-03 09:24:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 function run(){
-  console.log('hej');
+  console.log('hej2');
+  try{
   jsforce.browser.init({
-  clientId: 'test',
-  redirectUri: 'test',
-  proxyUrl: 'test'
+  clientId: '3MVG9xj60O9CjKHqAR6xaaX9u0JgiHcSo.P3c.i3Z6SRtOgiwUxIOQvy6EgSbassifvmoB8Gvt722lSqpIUq0',
+  redirectUri: 'https://test.salesforce.com'
 });
+  }
+  catch(error){console.error(error)}
 }
 
 run();

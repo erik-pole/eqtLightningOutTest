ce69f49a297b9f057c90bbac941b54a575b47904
erik-pole <101648330+erik-pole@users.noreply.github.com>
2024-04-03 09:36:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function run(){
-  console.log('hej3');
+  console.log('hej4');
   try{
     jsforce.browser.init({
       clientId: '3MVG9xj60O9CjKHqAR6xaaX9u0JgiHcSo.P3c.i3Z6SRtOgiwUxIOQvy6EgSbassifvmoB8Gvt722lSqpIUq0',
